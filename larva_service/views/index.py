9a8bb1eecbbc191a1c4918c9b67560a017684293
Kyle Wilcox <kyle@axiomalaska.com>
2015-06-09 14:22:30 -0400

    Put relevant environmental variables on index page

--- larva_service/views/index.py
+++ larva_service/views/index.py
@@ -2,14 +2,21 @@ from flask import render_template, make_response, redirect
 from larva_service import app
 from larva_service.views.helpers import requires_auth
 
+
 @app.route('/', methods=['GET'])
-def index():        
-    return render_template('index.html')
+def index():
+
+    show_keys = ['OUTPUT_PATH', 'SHORE_PATH', 'BATHY_PATH', 'CACHE_PATH', 'DEBUG', 'TESTING', 'NON_S3_OUTPUT_URL']
+    environ = { g: v for g, v in app.config.items() if g in show_keys }
+
+    return render_template('index.html', environment=environ)
+
 
 @requires_auth
 @app.route('/jobs', methods=['GET'])
 def jobs():        
     return redirect('/rq')
+
 
 @app.route('/crossdomain.xml', methods=['GET'])
 def crossdomain():
